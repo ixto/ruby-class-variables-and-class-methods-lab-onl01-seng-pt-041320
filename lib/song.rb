50bc0ab1f413e8caf506c736d3adfa1781926234
Joey <joey.alzamora@gmail.com>
2020-05-04 23:05:44 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song 
+  attr_accessor = :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+     @name = name
+     @artist = artist 
+     @genre = genre 
+       @@count = 3 
+       @@artists = []
+  end
+  
+   def name
+     @name
+   end 
+ 
+  def artist 
+    @artist
+  end 
+
+  def genre
+    @genre
+  end 
+
+  def self.count
+    @@count
+  end
+  
+  
+
+
+
+end
